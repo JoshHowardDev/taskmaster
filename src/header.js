834b97bd10e153c688127d5d17cda9469630843e
JoshH831 <joshhowardspring@gmail.com>
2022-10-20 16:37:02 -0400

    Remove header.js

--- src/header.js
+++
@@ -1,8 +0,0 @@
-import { generateHamburgerMenuDiv } from './navMenu.js'
-import { createEl } from './utilities.js'
-
-export function generateHeader() {
-    const header = createEl.div('header');
-    header.appendChild(generateHamburgerMenuDiv());
-    return header
-}
